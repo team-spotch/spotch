0fcabfe380f3b173121e402bff1fbcea339da48d
Yoshinori Iwasaki <wassyoi0531@gmail.com>
2016-07-19 16:17:56 +0000

    circle/showのview編集とcircle validation 編集

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -6,6 +6,17 @@ class Circle < ActiveRecord::Base
 	has_many :circle_members,through: :circle_users,source: :user
 	has_many :circle_events,dependent: :destroy
 
+	validate :add_error_sample
+
+	def add_error_sample
+		if name.empty?
+			errors[:base] << "サークル名を入力してください"
+		end
+		if desc.empty?
+			errors[:base] << "サークル概要を入力してください"
+		end
+	end
+
 	def circle_member?(circle,user)
 		circle_users.where(circle_id: circle.id,user_id: user.id).exists?
 	end
