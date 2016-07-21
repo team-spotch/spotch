71333a3819c52974eef065345bb0130c02c5b98c
Yoshinori Iwasaki <wassyoi0531@gmail.com>
2016-07-21 09:18:47 +0000

    サークルイベントのroutingをネストに変更してviewなどもそれに合わせるよう修正

--- app/controllers/circle_events_controller.rb
+++ app/controllers/circle_events_controller.rb
@@ -1,9 +1,12 @@
 class CircleEventsController < ApplicationController
 
 	before_action :authenticate_user!, except: :index
+	before_action :circle_index
+	before_action :event_authentication,only: [:new,:create,:update,:destroy]
 
 	def index
-		@circle_events = CircleEvent.all
+		@circle_events = CircleEvent.where(circle_id: params[:circle_id])
+
 	end
 
 	def show
@@ -16,9 +19,10 @@ class CircleEventsController < ApplicationController
 
 	def create
 		circle_event = CircleEvent.new(circle_event_params)
+		circle_event.circle_id = params[:circle_id] 
 
 		if circle_event.save
-			redirect_to circle_events_path,notice: "イベント作成に成功しました"
+			redirect_to circle_circle_events_path(@circle),notice: "イベント作成に成功しました"
 		else
 			redirect_to :new,alert: "イベント作成に失敗しました"
 		end
@@ -31,7 +35,7 @@ class CircleEventsController < ApplicationController
 	def update
 		@circle_event = CircleEvent.find(params[:id])
 		if @circle_event.update(circle_event_params)
-			redirect_to circle_event_path(params[:id]),notice: "イベント内容を更新しました"
+			redirect_to circle_circle_event_path(@circle,params[:id]),notice: "イベント内容を更新しました"
 		else
 			redirect_to :new,alert: "イベント内容の更新に失敗しました"
 		end
@@ -40,12 +44,22 @@ class CircleEventsController < ApplicationController
 	def destroy
 		@circle_event = CircleEvent.find(params[:id])
 		@circle_event.destroy
-		redirect_to circle_events_path,notice: "イベントを削除しました"
+		redirect_to circle_circle_events_path,notice: "イベントを削除しました"
 	end
 
 	private 
 	def circle_event_params
 		params.require(:circle_event).permit(:title,:desc,:event_date,:place,:one_phrase)
+	end
+
+	def circle_index
+		@circle = Circle.find(params[:circle_id])
+	end
+
+	def event_authentication
+		unless current_user.circle_users.where(circle_id: params[:circle_id]).exists?
+			redirect_to circle_circle_events_path
+		end
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :circles do
     resource :circle_users,only: [:create,:destroy]
-    
+    resources :circle_events
+
   end
   devise_for :users
-
-  resources :circle_events
 
 end
