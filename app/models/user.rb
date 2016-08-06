9c181999fe7036771218eb17ac7056a052ace4bd
Takara <yanshi_1334@outlook.com>
2016-08-06 06:16:32 +0000

    フォロー機能途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+   has_many :relationships, foreign_key: :follower_id
+   has_many :followings, through: :relationships
+
+   has_many :inverse_follows, foreign_key: :following_id, class_name: Relationship
+   has_many :followers, through: :inverse_follows
+
+   def followed_by? User
+   	inverse_follows.where(follower_id: user.id).exists?
+   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :users, only:[:index, :edit, :update, :show] do
+    resource :relationships, only:[:create, :destroy]
+    get :follows, on: :member
+    get :folowers, on: :member
+  end
+
+  get '/users' => 'users#index'
+  get '/users/:id' => 'users#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
