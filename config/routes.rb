eee09b1a6243abbd854db02154e179d05d967ce0
Yoshinori Iwasaki <wassyoi0531@gmail.com>
2016-09-11 07:35:18 +0000

    circleevent/showのview修正

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,6 @@ Rails.application.routes.draw do
 
 
   post "/circle_events/:id/talks" => "circle_event_talks#create",as: "circle_event_talks"
-
   post "/circles/:circle_id/circle_events/:id/attend" =>"circle_event_tickets#create",as: "circle_event_attend"
   delete "/circles/:circle_id/circle_events/:id/attend" =>"circle_event_tickets#destroy",as: "destroy_circle_event_attend"
   post "/circles/:circle_id/circle_events/:id/attend/:ticket_id" =>"circle_event_tickets#approve",as: "approve_circle_event_attend"
